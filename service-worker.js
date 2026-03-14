5b95d9154a8cabd91ba73451fb916d5656690202
Ilija994 <mirosavljevicilija18@gmail.com>
2026-03-14 15:27:55 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const CACHE = 'pt-v1';
+const FILES = ['./index.html', './web-app-manifest-512x512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
